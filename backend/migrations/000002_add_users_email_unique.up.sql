a981f8a5d147a0d0be789d3c8da37c3a6fc23544
Artem <90620853+k3llone@users.noreply.github.com>
2026-05-12 21:34:35 +0300

    fix: Добавил запуск миграций в docker compose

--- backend/migrations/000002_add_users_email_unique.up.sql
+++ backend/migrations/000002_add_users_email_unique.up.sql
@@ -1,2 +1,11 @@
-ALTER TABLE users
-ADD CONSTRAINT users_email_unique UNIQUE (email);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'users_email_unique'
+    ) THEN
+        ALTER TABLE users
+        ADD CONSTRAINT users_email_unique UNIQUE (email);
+    END IF;
+END $$;
